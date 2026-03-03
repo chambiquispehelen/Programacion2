1762ecd3ac01e9f92889729619dd016bbd697f4d
chambiquispehelen <chambiquispehelen@gmail.com>
2026-03-02 23:01:07 -0400

    arregle la ecuacioncuadratica

---
+++ LABORATORIO_01/mainCuadratica.py
@@ -0,0 +1,16 @@
+from EcuacionCuadratica import EcuacionCuadratica
+
+a = float(input("Ingrese a: "))
+b = float(input("Ingrese b: "))
+c = float(input("Ingrese c: "))
+
+ecuacion = EcuacionCuadratica(a, b, c)
+
+d = ecuacion.getDiscriminante()
+
+if d > 0:
+    print("La ecuación tiene dos raíces", ecuacion.getRaiz1(), "y", ecuacion.getRaiz2())
+elif d == 0:
+    print("La ecuación tiene una raíz", ecuacion.getRaiz1())
+else:
+    print("La ecuación no tiene raíces reales")
