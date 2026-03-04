1a0acf46a1f8768657308bc5f35be7f40dde8c67
chambiquispehelen <chambiquispehelen@gmail.com>
2026-03-03 21:52:29 -0400

    Ejer.Ecua.Cuadr

--- LABORATORIO_01/EcuacionCuadratica.py
+++ LABORATORIO_01/EcuacionCuadratica.py
@@ -1,34 +1,21 @@
-import math
 class EcuacionCuadratica:
 
-    # b) Constructor
     def __init__(self, a, b, c):
-
-        # a) Atributos privados
         self.__a = a
         self.__b = b
         self.__c = c
 
-    # c) Método que calcula el discriminante
-    def getDiscrim(self):
-        d = self.__b**2 - 4*self.__a*self.__c
-        return d
+    def getDiscriminante(self):
+        return self.__b**2 - 4*self.__a*self.__c
 
-    # d) Método para la primera raíz
     def getRaiz1(self):
-        d = self.getDiscrim()
-
-        if d >= 0:
-            r1 = (-self.__b + math.sqrt(d)) / (2*self.__a)
-            return r1
-        else:
+        d = self.getDiscriminante()
+        if d < 0:
             return 0
+        return (-self.__b + d**0.5) / (2*self.__a)
 
-    # d) Méodo para la segunda raíz
     def getRaiz2(self):
-        d = self.getDiscrim()
-        if d >= 0:
-            r2 = (-self.__b - math.sqrt(d)) / (2*self.__a)
-            return r2
-        else:
+        d = self.getDiscriminante()
+        if d < 0:
             return 0
+        return (-self.__b - d**0.5) / (2*self.__a)
--- LABORATORIO_01/mainEcuacion.py
+++ LABORATORIO_01/mainEcuacion.py
@@ -1,16 +1,26 @@
-from EcuacionLineal import EcuacionLineal
+from EcuacionCuadratica import EcuacionCuadratica
 
 a = float(input("Ingrese a: "))
 b = float(input("Ingrese b: "))
 c = float(input("Ingrese c: "))
-d = float(input("Ingrese d: "))
-e = float(input("Ingrese e: "))
-f = float(input("Ingrese f: "))
 
-ecua = EcuacionLineal(a, b, c, d, e, f)
-
-if ecua.tieneSolucion():
-    print("x =", ecua.getX())
-    print("y =", ecua.getY())
+# validar ecuación cuadrática
+if a == 0:
+    print("No es una ecuación cuadrática")
 else:
-    print("La ecuación no tiene solución")
+    ecuacion = EcuacionCuadratica(a, b, c)
+
+    d = ecuacion.getDiscriminante()
+
+    if d > 0:
+        print("La ecuación tiene dos raíces",
+              f"{ecuacion.getRaiz1():.6f}",
+              "y",
+              f"{ecuacion.getRaiz2():.6f}")
+
+    elif d == 0:
+        print("La ecuación tiene una raíz",
+              f"{ecuacion.getRaiz1():.6f}")
+
+    else:
+        print("La ecuación no tiene raíces reales")
